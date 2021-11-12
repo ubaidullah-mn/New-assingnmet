e5bdaf885830cf8460d5bb859e3a7f998ba8f19e
Ubaiduulah <ubaidullah5150@gmail.com>
2021-11-12 19:26:17 +0500

    Work Accomplished

---
+++ app.js
@@ -0,0 +1,37 @@
+  //   chap: MATHS METHOD
+  //    ques 1:
+ //var num = prompt("Enter a Positive Number") ;
+//document.write("Number = " +  num + "<br>")
+ //var num1 = Math.round(num);
+ //document.write("Round = " +  num1 + "<br>")
+ //var num2 = Math.floor(num);
+ //document.write("Floor = " +  num2 + "<br>")
+ //var num3 = Math.ceil(num);
+ //document.write("Ceil = " +  num3 + "<br>")
+//     ques 2:
+ //var num = prompt("Enter a Negative Number") ;
+//document.write("Number = " +  num + "<br>")
+// var num1 = Math.round(num);
+// document.write("Round = " +  num1 + "<br>")
+ //var num2 = Math.floor(num);
+ //document.write("Floor = " +  num2 + "<br>")
+ //var num3 = Math.ceil(num);
+ //document.write("Ceil = " +  num3 + "<br>")
+ //    ques 4:
+ //var num1  = Math.random() * 10 + 1
+ //var num2  = Math.random() * 20 + 1
+ //num1 = Math.floor(num1)
+ //num2 = Math.floor(num2)
+ //document.write("random dice vale :" + num1 + "<br>" )
+ //document.write("random dice vale :" + num2 + "<br>" )
+ //    ques 5:
+ //var num1  = Math.random() * 2 + 1
+ //var num2  = Math.random() * 3 + 1
+ //num1 = Math.floor(num1)
+ //num2 = Math.floor(num2)
+ //document.write(num1 + "<br>" + "random coin value :Head" + "<br>" )
+ //document.write(num2 + "<br>" + "random coin value :Tail" + "<br>" )
+ //    ques 6:
+ //var num  = Math.random() * 100 + 1 
+ //num = Math.floor(num)
+ //document.write("Random number between 1 & 100 :" + num + "<br>" )
